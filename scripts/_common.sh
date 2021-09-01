2b9fd7d78936bf702670dfbf49534c8e6f8342df
ericgaspar <junk.eg@free.fr>
2021-09-01 14:05:49 +0200

    First commit

---
+++ scripts/_common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#=================================================
+# COMMON VARIABLES
+#=================================================
+
+# dependencies used by the app
+pkg_dependencies="libicu-dev python3-venv python3-icu pkg-config python3 python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools"
+
+libretranslate_version="1.2.3"
+
+#=================================================
+# PERSONAL HELPERS
+#=================================================
+
+#=================================================
+# EXPERIMENTAL HELPERS
+#=================================================
+
+#=================================================
+# FUTURE OFFICIAL HELPERS
+#=================================================
