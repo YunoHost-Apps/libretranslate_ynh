cb902ed1925365204276f51922594510002776b5
ericgaspar <junk.eg@free.fr>
2021-12-09 16:13:43 +0100

    1.2.5

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="libicu-dev python3-venv python3-icu pkg-config python3 python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools"
 
-libretranslate_version="1.2.3"
+libretranslate_version="1.2.5"
 
 #=================================================
 # PERSONAL HELPERS
