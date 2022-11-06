bbd88018c44d387edf0ecd0ee4296d225b69723c
ericgaspar <junk.eg@free.fr>
2022-11-06 08:50:27 +0100

    1.3.0

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 # dependencies used by the app
 pkg_dependencies="libicu-dev python3-venv python3-icu pkg-config python3 python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools"
 
-libretranslate_version="1.2.7"
+libretranslate_version="1.3.0"
 
 #=================================================
 # PERSONAL HELPERS
