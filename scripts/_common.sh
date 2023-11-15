cf61bcb21257dfc4adf3953191438f7aac289c65
Thomas <51749973+Thovi98@users.noreply.github.com>
2023-11-15 20:35:12 +0100

    upgrade to 1.5.2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,7 +4,7 @@
 # COMMON VARIABLES
 #=================================================
 
-libretranslate_version="1.5.1"
+libretranslate_version="1.5.2"
 
 #=================================================
 # PERSONAL HELPERS
