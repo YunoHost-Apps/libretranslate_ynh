c0d61e982effcb3319ec3f3ee0781b3c0b1d604a
Félix Piédallu <felix@piedallu.me>
2025-01-07 15:19:02 +0100

    Cleanup, install as app, fixes

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,12 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS
 #=================================================
+
+install_via_venv() {
+
+    pushd "$install_dir"
+        ynh_exec_as_app python3 -m venv "$install_dir/venv"
+        ynh_hide_warnings ynh_exec_as_app "$install_dir/venv/bin/pip" install \
+            libretranslate=="$(ynh_app_upstream_version)" toml
+    popd
+}
