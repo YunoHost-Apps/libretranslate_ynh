18c8cc18c73b3953a48262a19a4b5b9f31ca2731
Félix Piédallu <felix@piedallu.me>
2025-01-07 15:59:03 +0100

    Remove pip cache to reduce backup size

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -10,5 +10,7 @@ install_via_venv() {
         ynh_exec_as_app python3 -m venv "$install_dir/venv"
         ynh_hide_warnings ynh_exec_as_app "$install_dir/venv/bin/pip" install \
             libretranslate=="$(ynh_app_upstream_version)" toml
+
+        ynh_safe_rm "$install_dir/.cache/pip"
     popd
 }
